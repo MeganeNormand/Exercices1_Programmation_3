80c4c0ab6caed0540a99cfb8a27581754367e1f7
Megane Normand <m@gmail.com>
2022-08-23 14:31:17 -0400

    Ajout du projet1 dans le répertoire

---
+++ IdeaProjects/premierProjet/src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("J'aime les patates");
+        System.out.println( "Hello World!" );
+    }
+}
